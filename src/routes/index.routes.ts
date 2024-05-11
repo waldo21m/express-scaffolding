fe61d90b410476a7b737df2cd308c162518eafee
Eduardo Márquez <eduardo20.3263@gmail.com>
2024-05-11 11:59:31 -0400

    Min changes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import { Router } from 'express';
-import userRouter from './user.routes';
+import userRoutes from './user.routes';
 
 dotenv.config();
 const router = Router();
 
 const prefix = `${process.env.APP_URI}${process.env.APP_VERSION}`
 
-router.use(prefix + '/users', userRouter);
+router.use(prefix + '/users', userRoutes);
 
 export default router;
